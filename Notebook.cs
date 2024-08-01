8e5c635483550386e2e60d8977083c13bc78d933
Koena <Koenapoopedi4.0@gmail.com>
2024-08-01 13:58:31 +0200

    new updates

---
+++ Notebook.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotebookApp
+{
+    class Notebook
+    {
+        public const string IntroMessage = "Welcome to Notebook App v1";
+        public const string OutroMessage = "Thanks for using Notebook App v1"; 
+
+        List<IPageable> pages = new List<IPageable>();
+
+        public delegate void SimpleFunction(string command);
+        private Dictionary<string, SimpleFunction>  commandLineArgs = new Dictionary<string, SimpleFunction>();
+        public readonly string show = "show", _new = "new", delete = "delete";//keys
+
+
+
+        public SimpleFunction this[string command]
+        { get
+            {
+                return commandLineArgs[command];
+            }
+        }
+
+        public Notebook()
+        { 
+            commandLineArgs.Add(show, Show);
+            commandLineArgs.Add(_new, New);
+            commandLineArgs.Add(delete, Delete);
+
+        }   
+
+        /// <summary>
+        /// Creates a new notebook with imput keywords for commands instead of default ones.
+        /// </summary>
+        /// <param name="commandLineKeywords"> index 0 = show, 1 = new, 2 = delete. </param>
+
+        public Notebook(params string[] commandLineKeywords) : this()
+        {
+            for (int i = 0; i < commandLineKeywords.Length; i++)
+            {   //if nothing is input jump to the next iteration
+                if (commandLineKeywords[i] == "")
+                {
+                    continue;
+                }
+                switch(i)
+                {
+                    //show
+                    case 0:
+                        commandLineArgs.Remove(show);
+                        commandLineArgs.Add(show = commandLineKeywords[i], Show);
+                        break;
+                    //new
+                    case 1:
+                        commandLineArgs.Remove(_new);
+                        commandLineArgs.Add(_new = commandLineKeywords[i], New);
+                        break ;
+                     //delete
+                    case 2:
+                        commandLineArgs.Remove(delete);
+                        commandLineArgs.Add(delete = commandLineKeywords[i], Delete);
+                        break ;
+                }
+            }
+        }
+
+        private   void New(string command)
+        {
+
+            switch (command)
+            {
+                case "":
+                    Console.WriteLine("New commands:");
+                    Console.WriteLine("message   create new message page");
+                    Console.WriteLine("list      create new list page");
+                    Console.WriteLine("image     create new image page");
+                    break;
+                case "message":
+                    Console.WriteLine("Creating message page!");
+                    pages.Add(new TextualMessage().Input());
+                    break;
+                case "list":
+                    Console.WriteLine("Creating list page!");
+                    pages.Add(new MessageList().Input());
+                    break;
+                case "image":
+                    Console.WriteLine("Creating image page!");
+                    break;
+            }
+        }
+        private void Show(string command)
+        {
+           switch(command)
+            {
+                case "":
+                    Console.WriteLine("Show commands:");
+                    Console.WriteLine("pages        lists all created pages");
+                    Console.WriteLine("id of page   display that page");
+                    break;
+
+                case "pages":
+                    Console.WriteLine("Showing all pages!");
+                    for(int i = 0; i < pages.Count; ++i)
+                    {
+                        Console.WriteLine("ID: " + i + " " + pages[i].MyData.tittle);
+                    }
+                    break;
+                default:
+                    int number;
+
+                    if (int.TryParse(command, out number)) 
+                    {
+
+                        Console.WriteLine("Showing page" + number);
+
+                        if(number < pages.Count)
+                        {
+                            pages[number].Output();
+                        }
+                    }
+                    break;
+
+            }
+        }
+       
+        private void Delete(string command)
+        {
+            switch (command)
+            {
+                case "":
+                    Console.WriteLine("Delete commands:");
+                    Console.WriteLine("all        lists all created pages");
+                    Console.WriteLine("id of page   display that page");
+                    break;
+
+                case "all":
+                    Console.WriteLine("Deleting all pages!");
+                    pages.Clear();
+                    break;
+                default:
+                    int number;
+
+                    if (int.TryParse(command, out number))
+                    {
+
+                        Console.WriteLine("Deleting page" + number);
+                        
+                        if (number < pages.Count)
+                        {
+                            pages.RemoveAt(number);
+                        }
+                    }
+                    break;
+
+            }
+        }
+    }
+}
---
+++ TextualMessage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotebookApp
+{
+    class TextualMessage : IPageable
+    {
+        protected PageData myData;
+        protected string message;
+      
+        public virtual IPageable Input()
+        {
+            Console.WriteLine("Please input your name");
+            myData.Author = Console.ReadLine();
+            Console.WriteLine("Plase input the message tittle");
+            myData.tittle = Console.ReadLine();
+            Console.WriteLine("Please input a message");
+            message = Console.ReadLine();
+
+            return this;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine();
+            Console.WriteLine("/------------------Message-----------------\\");
+            Console.WriteLine("Title: " + myData.tittle);
+            Console.WriteLine("Author: " + myData.Author);
+            Console.WriteLine("Message: \n \n" + message);
+            Console.WriteLine("\\-----------------------------------------/");
+           
+        }
+        public PageData MyData
+        { get{ 
+                return myData;
+             }
+            set {
+                myData = value;
+            }
+        }
+    }
+}
